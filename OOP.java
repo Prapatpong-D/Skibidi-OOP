2cdbb63970ad2569cef1d74638061008fc7ef6f3
Tae.- <67070097@kmitl.ac.th>
2024-12-16 20:12:48 +0700

    Week 3 ข้อ 9.

---
+++ OOP.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class OOP {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Hello to");
+        System.out.println("Hello to2");
+        System.out.print("Hello to3");
+        String string = sc.nextLine();
+    }
+}
